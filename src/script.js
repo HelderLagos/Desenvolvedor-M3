721022741285dd991432c7679a444e332447174d
Helder Lagos <helderlagospro@gmail.com>
2022-12-04 15:46:51 -0300

    db.json sincronizado com a galeria via js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,19 @@
 import produtos from './JS/db.json' assert {type: 'json'};
 
-let nameProd = produtos.products.map(produto => console.log(produto.price))
+
+const showcase = () => {
+    let containerProducts = document.querySelector('.main-container');
+    produtos.products.map((val)=>{
+        containerProducts.innerHTML += `
+        <div class="product">
+        <img src="${val.image}" alt="">
+        <h3>${val.name}</h3>
+        <span>R$ ${val.price}</span>
+        <span class="parcela">até ${val.parcelamento}</span>
+        <button key=${val.id}>COMPRAR</button>
+        </div>`
+    })
+}
+
+showcase();
 
