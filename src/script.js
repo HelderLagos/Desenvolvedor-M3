c20d05a82b9069a000d80e8e283676233a10d281
Helder Lagos <helderlagospro@gmail.com>
2022-12-05 13:28:31 -0300

    função de adição ao carrinho de compras ok

--- src/script.js
+++ src/script.js
@@ -1,19 +1,36 @@
 import produtos from './JS/db.json' assert {type: 'json'};
 
+let containerProducts = document.querySelector('.main-container');
 
-const showcase = () => {
-    let containerProducts = document.querySelector('.main-container');
-    produtos.products.map((val)=>{
+const showcase = (array) => {
+    containerProducts.innerHTML = ""
+    array.map((val)=>{
         containerProducts.innerHTML += `
         <div class="product">
-        <img src="${val.image}" alt="">
+        <img src="${val.image}" alt="${val.name}">
         <h3>${val.name}</h3>
-        <span>R$ ${val.price}</span>
+        <span class="item-price">R$ ${val.price}</span>
         <span class="parcela">até ${val.parcelamento}</span>
-        <button key=${val.id}>COMPRAR</button>
+        <a key="${val.id}" class="add-to-cart">COMPRAR</a>        
         </div>`
     })
 }
 
-showcase();
+showcase(produtos.products);
 
+const buttonCart = document.querySelector('.add-to-cart')
+let numItensCart = document.querySelector('.num-cart')
+let cart = 0
+
+const addToCart = (event) => {    
+    if (event.target.tagName == "A"){
+        cart++
+        numItensCart.innerHTML = cart
+    }
+}
+
+const filterProd = () => {
+    
+}
+
+containerProducts.addEventListener('click', addToCart );
